ad94e22d27abdf9e4f9ffa1dac2f781a202b4680
Victoria Marques <vicck.rj@gmail.com>
2020-09-09 11:23:49 -0300

    add database

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const sqlite = require('sqlite3').verbose();
-const db = new sqlite.Database('./toDoDB');
+const db = new sqlite.Database('./database/toDoDB');
 
 db.serialize( () => {
-    // db.run('CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, description TEXT)');
+    db.run('CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, description TEXT)');
     db.run('INSERT INTO tasks (id, title, description) VALUES (1, "Programming", "Do logic exercises")', (err) => {
         if(err) { console.log(err.message) }
     });
